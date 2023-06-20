4a412a8083becdb4d8f9e626673f4c50c880a209
tunaegilmez <tunaegilmez07@gmail.com>
2023-06-20 17:50:40 +0300

    UI Done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="text-accent-content">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style>
@@ -22,7 +17,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
 
-Vue.config.productionTip = false
+import "./index.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
+  router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount("#app");
